12e7062377cdc2c4ef0becfd283989eba6e2eb00
ZAID ADIL <hothead01th@gmail.com>
2025-02-16 23:46:37 +0530

    changed builder page ui

--- components/form/FormCP.jsx
+++ components/form/FormCP.jsx
@@ -5,7 +5,7 @@ const FormCP = ({ formClose, setFormClose }) => {
   return (
     <button
       aria-label="Form Open/Close"
-      className="exclude-print fixed bottom-5 left-10 font-bold rounded-full bg-white text-[rgb(5,234,250)] shadow-lg border-2 border-white"
+      className={`exclude-print fixed bottom-5 ${formClose ? 'left-5' : 'left-1/3 -translate-x-1/2'} font-bold rounded-full bg-white text-zinc-800 shadow-lg border-2 border-white z-50 hidden md:block`}
       onClick={() => setFormClose(!formClose)}
     >
       {formClose ? <BsFillArrowRightCircleFill className="w-10 h-10" title="Form Open" /> : <BsFillArrowLeftCircleFill className="w-10 h-10" title="Form Close" />}
